11afd234a24eef6944dcc08d4f3d9822b713b9a6
dev-woong <dev-woong>
2020-09-14 20:56:09 +0900

    modify : delete proxy

---
+++ build.tar/precache-manifest.c1310f1659e4cae99bf83ba0effaadf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b096a14b11ead4c9da0bfc520cf17857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b8b3c592b281648df6",
+    "url": "/static/js/2.3c8eb332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c8eb332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0108d4ae5ba15dc7edb3",
+    "url": "/static/js/main.d7a53c4e.chunk.js"
+  },
+  {
+    "revision": "5e7195dae9b534a2638a",
+    "url": "/static/js/runtime-main.c749d85f.js"
+  }
+]);
